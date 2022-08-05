4d30c3f4d160c9de1d2f8a25f5897f10ee6c4111
rocio2504 <rocioandrea250489@gmail.com>
2022-08-05 17:25:47 -0500

    Se cambio el nombre del archivo .js y se creo 2 funciones para cada boton

---
+++ main.js
@@ -0,0 +1,11 @@
+function prender_foco(){
+
+document.getElementById('bulb').src= 'https://i.pinimg.com/236x/9b/5d/38/9b5d38f0884bbef6b391b66cdc452fd5.jpg'
+
+}
+
+function apagar_foco(){
+
+document.getElementById('bulb').src= 'https://www.w3schools.com/js/pic_bulboff.gif'
+
+}
